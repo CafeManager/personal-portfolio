9db7b841ab6fc56ad86318413f756a47a169588f
CafeManager <kevinflores1997@yahoo.com>
2024-02-04 21:24:54 -0800

    added landing page

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This is my own personal site.
+
+Color palette
+Rich black - 0B2027
+Cerulean - 40798c
+Verdigris - 70A9A1
+Ash gray - CFD7C7
+Cornsilk - F6F1D1
---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App h-100">
+            <div className="container h-100 d-flex align-items-center">
+                <div className="mx-auto container-sizing ">
+                    <div className="mr-auto ms-5 my-5">
+                        <div className="text-start">
+                            <h1 className="mr-auto"> Welcome </h1>
+                            <p>
+                                {" "}
+                                I am Kevin Flores. At vero eos et accusamus et
+                                iusto odio dignissimos ducimus qui blanditiis
+                                praesentium voluptatum deleniti atque corrupti
+                                quos dolores et quas molestias excepturi sint
+                                occaecati cupiditate non provident, similique
+                                sunt in culpa qui officia deserunt mollitia
+                                animi, id est laborum et dolorum fuga. Et harum
+                                quidem rerum facilis est et expedita distinctio.
+                                Nam libero tempore, cum soluta nobis est
+                                eligendi optio cumque nihil impedit quo minus id
+                                quod maxime placeat facere possimus, omnis
+                                voluptas assumenda est, omnis dolor repellendus.
+                                Temporibus autem quibusdam et aut officiis
+                                debitis aut rerum necessitatibus saepe eveniet
+                                ut et voluptates repudiandae sint et molestiae
+                                non recusandae. Itaque earum rerum hic tenetur a
+                                sapiente delectus, ut aut reiciendis
+                                voluptatibus maiores alias consequatur aut
+                                perferendis doloribus asperiores repella
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
